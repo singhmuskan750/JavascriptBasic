c3ec84eeff90b9f3d232a2845005e91f03276eb2
singhmuskan750 <singhmuskan0099@gmail.com>
2021-09-02 19:31:46 +0530

    console basics done

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('Hey this my first piece of JS code!! yay')
+console.log(1546121161)
+console.log({name: "Muskan"})
+console.log(true)
+
+console.warn("This is a warning")
+
+console.error("This is an error")
