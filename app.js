373cbb96168a91f43ce857ff8efe6d7ee479aa68
singhmuskan750 <singhmuskan0099@gmail.com>
2021-09-10 19:04:16 +0530

    DOM selectors

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-// Data SVGUnitTypes
-// Primitive or Non Primitive
-
-let age = 1923.1549862;
-let Name = 'asdf'
-
-// Non Primitive data type
-
-let arr = [1, 2, 3, 4, 5]
-arr.push("value")
-console.log(arr)
-console.log(typeof arr)
+// const element = document.getElementById('selectme')
+// const element = document.getElementsByClassName('heading')
+// const element = document.getElementsByTagName('h3')
+const element =  document.querySelector('h3.heading')
+console.log(element)
