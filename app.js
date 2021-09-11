11a465812244377469216283914c7c3234f5ddad
singhmuskan750 <singhmuskan0099@gmail.com>
2021-09-11 21:25:35 +0530

    array method

--- app.js
+++ app.js
@@ -1,35 +1,11 @@
-const rock = document.querySelector('#rock')
-const paper = document.querySelector('#paper')
-const scissor = document.querySelector('#scissor')
-const board = document.querySelector('#board')
+const balls =  document.getElementsByClassName('ball')
+const body = document.querySelector('body')
+// console.log('balls')
 
-const handler = (event) => {
-    const action = event.target.id
-    // if(action==='rock'){
-    //     board.innerHTML=("Rock crushes scissor")
-    // }else if(action==='paper'){
-    //     board.innerHTML=("Paper traps rock")
-    // }else if(action=='scissor'){
-    //     board.innerHTML=("Scissor cuts paper")
-    // }else {
-    //     board.innerHTML=("Click something")
-    // }
+const arr = [1, 2, 3, 4, 5]
+// arr.push(6)
+// console.log(arr)
 
-    switch(action){
-        case "rock":
-            board.innerHTML=("Rock crushes scissor")
-            break
-        case "paper":
-            board.innerHTML=("Paper traps rock")
-            break
-        case "scissor":
-            board.innerHTML=("Click something")
-            break
-        default:
-            board.innerHTML=("Click something")
-            break
-    }
+for (var i = 0;i<arr.length;i++){
+    console.log(arr[i])
 }
-rock.addEventListener('click',handler)
-paper.addEventListener('click', handler)
-scissor.addEventListener('click',  handler)
