3fcc50cfe127fee5823a616282f6a0ffba186b5f
singhmuskan750 <singhmuskan0099@gmail.com>
2021-09-12 20:22:12 +0530

    filter array

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-let ranks = [1, 2, 3, 4];
+let ranks = [1, 2, 3, 4, 5, 6];
 
 console.log(
     ranks.map((e, index) => {
         return e * 100;
     })
 );
-
-let newarr = ranks.map((e, index) => {
+// map Array
+let newmaparr = ranks.map((e, index) => {
     if(e%2===0)
         return e
 })
-console.log(newarr);
+console.log(newmaparr);
+
+// filter Array
+
+let newfilterarr = ranks.filter((e, index) => {
+    if(e%2===0)
+        return e
+})
+console.log(newfilterarr);
